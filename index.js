e95dca5bfc36d6c6b79e9b1c1890775df2d5b0d4
4313657 <4313657@TE-GDA-D-D21E01>
2025-04-04 09:27:30 +0200

    zadanie pozdrawiam cieplutko z rodzina

--- index.js
+++ index.js
@@ -9,21 +9,25 @@ const client = new pg.Client({
 });
 
 const getFromDb = async () => {
-    await client.connect();
-
     const result = await client.query('SELECT * FROM grades');
+    console.log(result.rows);
     console.table(result.rows);
-};
-
-getFromDb();
-
-const insertToDb = async (student_id, course_id, grade) => {
-        const insertQuery = 'INSERT INTO grades (student_id, course_id, grade) VALUES ($1, $2, $3) RETURNING *';
-        const values = [student_id, course_id, grade];
-        const result = await client.query(insertQuery, values); 
-        console.table(result.rows); 
-
-        
 }
 
-insertToDb(1, 1, 5);
+const insertToDb = async (student_id, course_id, grade) => {
+    const result = await client.query(
+        'INSERT INTO grades(student_id, course_id, grade) VALUES($1, $2, $3)', 
+        [student_id, course_id, grade] 
+    );
+    
+    console.log(result);
+}
+
+const main = async () => {
+    await client.connect();
+    await insertToDb(1, 1, 5);
+    await getFromDb();
+    await client.end();
+}
+
+main();
